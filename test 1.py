68b6d4be53dbd1d8950a757f897779d10e939f97
Johan Östling <joos@wappo.se>
2023-05-08 14:25:35 +0200

    Linjediagram v1

--- test 1.py
+++ test 1.py
@@ -1,1 +1,16 @@
-print("hej")
+import matplotlib.pyplot as plt
+
+# Skapa data för x- och y-koordinaterna
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Rita linjediagrammet
+plt.plot(x, y)
+
+# Ange etiketter för axlarna och titel för diagrammet
+plt.xlabel('X-axel')
+plt.ylabel('Y-axel')
+plt.title('Linjediagram')
+
+# Visa diagrammet
+plt.show()
